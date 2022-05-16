8f1d34687ed9cf34df62b6843b549c8e33954cff
Selam Gabriel <selamgabriel@Selams-iMac.hsd1.tx.comcast.net>
2022-05-16 18:43:48 -0500

    Testing components

--- src/typescript/views/home/home.tsx
+++ src/typescript/views/home/home.tsx
@@ -1,11 +1,28 @@
 import * as React from "react"
+import { Chip } from "../../components/chip"
+import { Dropdown } from "../../components/dropdown"
+import { Scope } from "../../components/scope"
+import { Axis, Scrollview } from "../../components/scrollview"
+import { Slider } from "../../components/slider"
+import { Stepper } from "../../components/stepper"
+import { Textfield, textfieldType } from "../../components/textfield"
 
 
 
 function Home() : JSX.Element
 {
     return (
-        <h1>Hello World</h1>
+        <Scrollview axes={ Axis.vertical } content=
+        {
+        <>
+            <Chip label={""} />
+            <Dropdown />
+            <Scope />
+            <Slider />
+            <Stepper value={ 50 } max={ 100 } />
+            <Textfield type={ textfieldType.text } placeholder={ "Testing placeholder" } />
+        </>
+        } />
     )
 }
 
